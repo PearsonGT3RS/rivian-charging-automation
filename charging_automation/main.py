b72187f6edef898bd1bac3802e96b967f29bb2ec
PearsonGT3RS <bepear02@gmail.com>
2026-04-21 16:22:42 -0400

    find exception using traceback

--- charging_automation/main.py
+++ charging_automation/main.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import time
+import traceback # <--- NEW: Import the traceback library
 from ChargingAutomation import run_charging_automation
 
 
@@ -24,11 +25,22 @@ def main():
 
     while True:
         try:
-            run_charging_automation()
+            is_actively_charging = run_charging_automation()
+            
+            if is_actively_charging:
+                time.sleep(iteration_time)  
+            else:
+                time.sleep(1800) 
+                
         except Exception as e:
-            logger.error(f"An error occurred: {e}")
-        logger.info('Sleeping for {} seconds...'.format(iteration_time))
-        time.sleep(iteration_time)
+            # --- NEW: Print the full stack trace to the logs ---
+            logger.error(f"Automation crashed: {e}")
+            logger.error(traceback.format_exc())
+            logger.info('Sleeping for {} seconds...'.format(iteration_time))
+            time.sleep(iteration_time)
+            run_charging_automation()
+        
+
 
 
 if __name__ == '__main__':
